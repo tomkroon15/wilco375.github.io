1f9e0c4bad1ca7ab6c517e73f41a8857d5584f60
wilco375 <wilcovanbeijnum@gmail.com>
2016-02-24 13:05:06 +0100

    Added adblock detector, updated spellcraft

--- downloads/downloads.js
+++ downloads/downloads.js
@@ -1,3 +1,17 @@
+function adBlockNotDetected() {
+}
+
+function adBlockDetected() {
+    alert("AdBlock was detected. Please consider supporting me and my work by disabling AdBlock or adding this website to an exception filter");
+}
+
+if(typeof fuckAdBlock === "undefined") {
+    adBlockDetected();
+} else {
+    fuckAdBlock.onDetected(adBlockDetected);
+    fuckAdBlock.onNotDetected(adBlockNotDetected);
+}
+
 function onMobileNavbarClick(){
 	var navbar = document.getElementById("navigation_bar");
 	if(navbar.style.display == "none"){
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function adBlockNotDetected() {
+}
+
+function adBlockDetected() {
+    alert("AdBlock was detected. Please consider supporting me and my work by disabling AdBlock or adding this website to an exception filter");
+}
+
+if(typeof fuckAdBlock === "undefined") {
+    adBlockDetected();
+} else {
+    fuckAdBlock.onDetected(adBlockDetected);
+    fuckAdBlock.onNotDetected(adBlockNotDetected);
+    fuckAdBlock.on(true, adBlockDetected);
+    fuckAdBlock.on(false, adBlockNotDetected);
+}
+
 function onMobileNavbarClick(){
 	var navbar = document.getElementById("navigation_bar");
 	if(navbar.style.display == "none"){
